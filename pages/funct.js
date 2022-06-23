be75b1f69489af1677e829271b1841c16cad8cbe
mdouglas630 <48533555+mdouglas630@users.noreply.github.com>
2022-06-23 08:40:24 -0300

    Add files via upload

---
+++ pages/funct.js
@@ -0,0 +1,14 @@
+function Table(){
+    return(
+        <table>
+            <thead>
+                <th>nome</th>
+            </thead>
+            <tbody>
+                <tr>lucas</tr>
+            </tbody>
+        </table>
+    )
+}
+
+module.exports = {Table};
---
+++ pages/index.js
@@ -0,0 +1,20 @@
+const tab = require("./funct")
+export default function Home(){
+
+    return (
+ 
+       <div>
+
+           <tab.Table/>
+ 
+          <h2>
+
+            Viva Santana!
+ 
+          </h2>
+ 
+       </div>
+ 
+    )
+ 
+ }
